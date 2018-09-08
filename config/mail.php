8951e0f245c4bc116d2b8ed9ddd121b044f2d912
Mohamed Samir <gm.mohamedsamir@gmail.com>
2018-09-08 16:06:32 +0200

    first commit

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'fb.mohamedsamir@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'test@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Subscribe'),
     ],
 
